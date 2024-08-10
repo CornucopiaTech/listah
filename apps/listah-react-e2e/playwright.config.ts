21de06975b0cc4dda936886f98025e09f1ddf59a
Kelechi  Berquist <28319310+KelechiBerquist@users.noreply.github.com>
2024-08-10 14:58:25 -0400

    Feat/log api requests (#3)
    * refactor: change to react app
    
    * feat: record request in database

--- apps/listah-react-e2e/playwright.config.ts
+++ apps/listah-react-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn nx serve frontend',
+    command: 'yarn nx serve listah-react',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
