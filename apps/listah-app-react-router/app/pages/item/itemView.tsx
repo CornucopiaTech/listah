038c71ce0726f5ad832a24f7f01e2768b293a887
Kelechi  Berquist <28319310+KelechiBerquist@users.noreply.github.com>
2025-01-13 19:48:30 -0500

    Chore/conflict resolution (#12)
    * chore: eod commit after react router setup
    
    * chore: eod commit in between setting up reducer for listing
    
    * chore: eod commit while adding redux to item view
    
    * chore: resolve merge conflict

--- apps/listah-app-react-router/app/pages/item/itemView.tsx
+++ apps/listah-app-react-router/app/pages/item/itemView.tsx
@@ -1,8 +1,10 @@
 
 // import * as React from 'react';
-// import {
+import {
+	useContext,
+	Fragment
 
-// } from 'react';
+} from 'react';
 import {
     Box,
     Stack,
@@ -16,136 +18,102 @@ import {
     IconButton,
     Tooltip,
     Grid2 as Grid,
-    TextField
+    TextField,
+	ListItem,
  } from '@mui/material';
 
 import {
     Send,
     Delete,
     Add,
+	ExpandLess,
 } from '@mui/icons-material';
 
+
+
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '~/store';
 import {
-	useSelector,
-	useDispatch
-} from 'react-redux'
+ClickItem,
+ToggleCollapseTags,
+selectItemView,
+ChangeTags,
+	ChangeStatus,
+	CreateNewTag,
+	SaveUpdatedItem
+} from '~/hooks/reducers/items/itemViewSlice';
+import type { TagChangePayloadInterface } from '~/hooks/reducers/items/itemViewSlice';
 
-
-import TextFieldsLabelled from '../../components/Form/TextFieldsLabelled';
-
-type ListItemType = {}
 
 export default function ItemView(props){
-	const [status, setStatus] = React.useState('viewing');
-	const [open, setOpen] = React.useState(true);
+	const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+	const useAppSelector = useSelector.withTypes<RootState>()
+	const dispatch = useAppDispatch();
 
-    const itemViewState = useSelector(state => state.itemFilter.value)
-	const itemListingState = useSelector(state => state.itemListing.value)
-
-    const dispatch = useDispatch()
+	const itemViewState = useAppSelector(selectItemView);
+	const displayViewId = itemViewState.selectedItem ? 'simple-popper' : undefined;
 
 
-	function handleTagAdd(){
+	function handleTagAdd() {
 		alert('Tag add button clicked');
-		setStatus('editing');
 	}
-
 	function handleDelete(e: React.MouseEvent<HTMLButtonElement>){
 		alert('Delete Button Clicked');
 		setStatus('deleting');
 	}
 
-	function handleSave(e: React.MouseEvent<HTMLButtonElement>){
-		alert('Save Button Clicked');
-		setStatus('viewing');
-	}
-
-	function handleEdit(e: React.MouseEvent<HTMLButtonElement>){
-		alert('Edit Button Clicked');
-		setStatus('editing');
-	}
-
-	function addFormField(itemLabel: string, itemValue: string){
-		return <TextFieldsLabelled
-					key={itemLabel + '-' + itemValue + '-formField'}
-					status={status}
-					label={itemLabel.toLowerCase()}
-					value={itemValue}
-					size='small'/>
-	}
-
-	function createTagList(givenItem: ListItemType){
-		// Add tags field.
-		return (
-			<React.Fragment>
-
-				<List
-						sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-						component="nav"
-						aria-labelledby="nested-list-subheader"
-						subheader={
-							<ListSubheader component="div" id="nested-list-subheader"
-											sx={{display: 'flex'}}>
-								<ListItemText primary="Tags" />
-
-								<Tooltip title="Add new tag">
-									<ListItemIcon onClick={handleTagAdd}>
-										<AddIcon />
-									</ListItemIcon>
-								</Tooltip>
-							</ListSubheader>
-						}
-					>
-						{
-							status == 'editing' &&
-							<TextFieldsLabelled
-								key={givenItem['summary'] + 'new-tag-formField'}
-								status={status}
-								label='New Tag'
-								value=''
-								size='small'/>
-						}
-					<Collapse in={open} timeout="auto" unmountOnExit>
-					<List component="div" disablePadding>
-						{givenItem['tags'].map((item) => (
-							<ListItemButton key={givenItem['summary'] + 'tag-' + item + 'formField'}>
-								<ListItemIcon>
-								{status == 'editing' && <DeleteIcon />}
-								</ListItemIcon>
-								<ListItemText primary={item} />
-							</ListItemButton>
-						))}
-					</List>
-					</Collapse>
-				</List>
-			</React.Fragment>
-
-		);
-	}
-
-	function createMidItems(givenItem: ListItemType){
-		let result: React.ReactNode[] = [];
-
-		// Add other fields.
-		Object.entries(givenItem).forEach(([akey, avalue]) => {
-			if (!['id', 'summary', 'tags'].includes(akey)){
-				result.push(addFormField(akey, avalue));
-			}
-		});
-
-		return result;
-	}
-
-	console.log(props.selected);
+	console.log("Selected Items: ");
+	console.log(itemViewState.selectedItem);
 
 	return (
-		<Grid size={{xs:12, sm:12,  md: 12, lg:5, xl: 5 }}>
-		{/* Item Display list.
-			This renders only if an item has been selected and the selected item was not filtered out by the most recent filter.
-		*/}
+		<Grid container
+				spacing={{ xs: 1, sm: 2, md: 3, lg: 3, xl: 3 }}
+				columns={{ xs: 2, sm: 6, md: 12 }}>
+			<Grid size='grow'>
+				{/* Item list grid */}
+				<List sx={{
+							width: '100%', p: 2,
+							maxHeight: { xs: 240, sm: 240, md: 360, lg: 720, xl: 840 },
+							overflow: 'auto',
+							bgcolor: 'cyan',
+							justifyContent: "center",
+							alignItems: "flex-start",
+							border: 1,
+							borderColor: 'rgba(50,50,50,0.3)',
+						}}>
+					{
+						// ToDo: Make a vertical stack where the summary contains selected columns for the category
+						// ToDo: Have a ticker button at the start of the list
+						// ToDO: Long press should bring a menu with quick options for editing an item (for example. removing it temporarily)
+						props.data.map((listItem: ItemModel) => (
+							<ListItem
+								key={listItem.id}
+								variant="outlined"
+								aria-describedby={displayViewId}
+								onClick={() => dispatch(ClickItem(listItem))}
+								size='large'>
+								<ListItemButton>
+									{
+										itemViewState.selectedItem &&
+										listItem.id == itemViewState.selectedItem.id &&
+										<ListItemIcon><ExpandLess /></ListItemIcon>
+									}
+									<ListItemText primary={listItem.title} />
+								</ListItemButton>
+							</ListItem>
+						))
+					}
+				</List>
+			</Grid>
+			{
+				// Component for viewing selected item.
+				// This renders only if an item has been selected and the selected item was not filtered out by the most recent filter.
 
-			<Box 	sx={{
-						// border: 1,
+				itemViewState.selectedItem &&
+				props.data.filter((item: ItemModel) => item.id == itemViewState.selectedItem.id).length != 0 &&
+				<Grid size={{xs:12, sm:12,  md: 12, lg:5, xl: 5 }}>
+					<Box sx={{
+							// border: 1,
 							bgcolor: 'pink',
 							width: '100%',
 							flexGrow: 1,
@@ -153,55 +121,170 @@ export default function ItemView(props){
 							justifyContent: 'center',
 							alignItems: 'center',
 						}}>
-				<Box 	component="form"
-					key={itemListingState.selectedItem.title + '-Box'}
-						sx={{ 	'& .MuiTextField-root': { m: 1, width: '100%', maxWidth: 300, },
-								width: '100%', border: 1, p:2,
-								borderColor: 'rgba(50,50,50,0.3)',
-								bgcolor: 'background.paper',
-								maxHeight: {xs:360, sm: 480, md: 600, lg: 720, xl: 840},
-								overflow: 'auto',
-							}}
-						noValidate
-						autoComplete="off">
-					{
-						// Add title field.
-						addFormField('title', itemListingState.selectedItem.title)
-					}
-					{
-						// Add other fields.
-						createMidItems(itemListingState.selectedItem)
-					}
-					{
-						// Add tags field.
-						createTagList(itemListingState.selectedItem)
-					}
-				</Box>
-				<Stack 	spacing={2} direction="row"
-						key={itemListingState.selectedItem.title + '-Buttons'}
-						sx={{
-								width: '100%',
-								justifyContent: 'space-around',
-								bgcolor: 'purple',
-								dislay:'inline-flex',
-								p:1,
-							}}>
-					<Button key='edit' size="small"
-							sx={{display: status == 'viewing' ? 'block' : 'none'}}
-							onClick={handleEdit}>
-						Edit
-					</Button>
-					<Button key='save' size="small"
-							sx={{display: status == 'editing' ? 'block' : 'none'}}
-							onClick={handleSave}>
-						Save
-					</Button>
-					<Button key='delete'
-							size="small" onClick={handleDelete}>
-						Delete
-					</Button>
-				</Stack>
-			</Box>
+						<Box 	component="form"
+								key={itemViewState.selectedItem.title + '-Box'}
+								sx={{
+									'& .MuiTextField-root': { m: 1, width: '100%', maxWidth: 300, },
+									width: '100%', border: 1, p: 2,
+									borderColor: 'rgba(50,50,50,0.3)',
+									bgcolor: 'background.paper',
+									maxHeight: { xs: 360, sm: 480, md: 600, lg: 720, xl: 840 },
+									overflow: 'auto',
+								}}
+								noValidate
+								autoComplete="off">
+								{/* ToDo: Fix updating item attributes. */}
+
+							{
+								// Add item note for editing mode
+								itemViewState.status === 'editing' &&
+								<Fragment>
+									<TextField
+										required
+										multiline
+										key={'TextField-title-' + itemViewState.selectedItem.title + '-formField'}
+										label="Title"
+										value={itemViewState.selectedItem.title}
+										onChange={(e) => dispatch(ChangeTags({ current: e.target.value, previous: itemViewState.selectedItem.title }))}
+										size='small'
+									/>
+									<TextField
+										required
+										multiline
+										key={'TextField-note-' + itemViewState.selectedItem.note + '-formField'}
+										label="Note"
+										value={itemViewState.selectedItem.note}
+										onChange={(e) => dispatch(ChangeTags({ current: e.target.value, previous: itemViewState.selectedItem.note }))}
+										size='small'
+									/>
+									<TextField
+										required
+										multiline
+										key={'TextField-description-' + itemViewState.selectedItem.description + '-formField'}
+										label="Description"
+										value={itemViewState.selectedItem.description}
+										onChange={(e) => dispatch(ChangeTags({ current: e.target.value, previous: itemViewState.selectedItem.description }))}
+										size='small'
+									/>
+								</Fragment>
+
+							}
+							{
+								// Add item note for non-editing mode
+								itemViewState.status !== 'editing' &&
+								<Fragment>
+									<TextField
+										disabled
+										required
+										multiline
+										key={'TextField-title-' + itemViewState.selectedItem.title + '-formField'}
+										label="Title"
+										value={itemViewState.selectedItem.title}
+										onChange={(e) => dispatch(ChangeTags({ current: e.target.value, previous: itemViewState.selectedItem.title }))}
+										size='small'
+									/>
+									<TextField
+										disabled
+										required
+										multiline
+										key={'TextField-note-' + itemViewState.selectedItem.note + '-formField'}
+										label="Note"
+										value={itemViewState.selectedItem.note}
+										onChange={(e) => dispatch(ChangeTags({ current: e.target.value, previous: itemViewState.selectedItem.note }))}
+										size='small'
+									/>
+									<TextField
+										disabled
+										required
+										multiline
+										key={'TextField-description-' + itemViewState.selectedItem.description + '-formField'}
+										label="Description"
+										value={itemViewState.selectedItem.description}
+										onChange={(e) => dispatch(ChangeTags({ current: e.target.value, previous: itemViewState.selectedItem.description }))}
+										size='small'
+									/>
+								</Fragment>
+
+							}
+							<List
+									// Add List for tags field.
+									sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+									component="nav"
+									aria-labelledby="nested-list-subheader"
+									subheader={
+										<ListSubheader component="div" id="nested-list-subheader"
+											sx={{ display: 'flex' }}>
+											<ListItemText primary="Tags" />
+
+											<Tooltip title="Add new tag">
+												<ListItemIcon onClick={() => dispatch(ChangeStatus("editing"))}>
+													<Add />
+												</ListItemIcon>
+											</Tooltip>
+										</ListSubheader>
+									}>
+								{
+									itemViewState.status == 'editing' &&
+									<TextField
+										required
+										multiline
+										key={'TextField-newTag-' + itemViewState.selectedItem.title + '-formField'}
+										label='New Tag'
+											value={itemViewState.newTag}
+											onChange={(e) => dispatch(CreateNewTag(e.target.value))}
+										size='small'
+									/>
+								}
+								<Collapse 	in={itemViewState.tagCollapsed}
+											timeout="auto" unmountOnExit
+											onClick={() => dispatch(ToggleCollapseTags())}>
+									<List component="div" disablePadding>
+										{
+											itemViewState.selectedItem.tags.map((tagItem: string) => (
+												<ListItemButton key={itemViewState.selectedItem.title + 'tag-' + tagItem + 'formField'}>
+													<ListItemIcon>
+														{
+															itemViewState.status == 'editing' &&
+															// ToDO: Fix delete button function.
+															<Delete onClick={() => dispatch(ChangeTags({ current: null, previous: tagItem }))} />
+														}
+													</ListItemIcon>
+													<ListItemText primary={tagItem} />
+												</ListItemButton>
+											))
+										}
+									</List>
+								</Collapse>
+							</List>
+
+						</Box>
+						<Stack spacing={2} direction="row"
+								key={itemViewState.selectedItem.title + '-Buttons'}
+								sx={{
+									width: '100%',
+									justifyContent: 'space-around',
+									bgcolor: 'purple',
+									dislay: 'inline-flex',
+									p: 1,
+								}}>
+							<Button key='edit' size="small"
+									sx={{ display: itemViewState.status == 'viewing' ? 'block' : 'none' }}
+									onClick={() => dispatch(ChangeStatus("editing"))}>
+								Edit
+							</Button>
+							<Button key='save' size="small"
+									sx={{ display: itemViewState.status == 'editing' ? 'block' : 'none' }}
+									onClick={() => dispatch(SaveUpdatedItem())}>
+								Save
+							</Button>
+							<Button key='delete'
+									size="small" onClick={handleDelete}>
+								Delete
+							</Button>
+						</Stack>
+					</Box>
+				</Grid>
+			}
 		</Grid>
 	);
 }
