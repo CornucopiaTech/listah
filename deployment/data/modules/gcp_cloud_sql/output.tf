39dcf836fb248a4340dfc92e6a95dcdde1b4b76f
KelechiBerquist <kelechi@berquist.tech>
2026-01-25 17:35:31 -0500

    chore: bug fix

--- deployment/data/modules/gcp_cloud_sql/output.tf
+++ deployment/data/modules/gcp_cloud_sql/output.tf
@@ -1,5 +1,5 @@
 output "db_client_cert" {
   value       = google_sql_ssl_cert.client_cert.cert
   description = "The client certs for the Cloud SQL instance"
-  sensitive = true
+  sensitive   = true
 }
