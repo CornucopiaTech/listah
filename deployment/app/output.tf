95d94e414a94a1412a53ffe732f787f78f016054
KelechiBerquist <kelechi@berquist.tech>
2026-01-27 19:04:42 -0500

    chore: use outputs

--- deployment/app/output.tf
+++ deployment/app/output.tf
@@ -1,5 +1,10 @@
-# output "db_client_cert" {
-#   value       = module.gcp_cloud_sql.db_client_cert
-#   description = "The client certs for the Cloud SQL instance"
-#   sensitive   = true
-# }
+output "repo_uri" {
+  value       = data.terraform_remote_state.data.outputs.artifact_repo_uri
+  description = "The uri of the artifact repository"
+  sensitive   = true
+}
+output "client_cert" {
+  value       = data.terraform_remote_state.data.outputs.db_client_cert
+  description = "The client certs for the Cloud SQL instance"
+  sensitive   = true
+}
