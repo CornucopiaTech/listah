5827b915cab26041e730d58587fd740b55e77e8d
KelechiBerquist <kelechi@berquist.tech>
2026-07-26 16:21:42 -0400

    chore: wrap up

--- apps/web/src/hooks/context/filters/index.ts
+++ apps/web/src/hooks/context/filters/index.ts
@@ -1,6 +1,5 @@
 
 export * from "./useFilter";
 export * from "./queries";
-export * from "./FilterItemListProvider";
 export * from "./FilterListProvider";
 export * from "./FilterUpdateProvider";
--- apps/web/src/hooks/context/tags/index.ts
+++ apps/web/src/hooks/context/tags/index.ts
@@ -2,5 +2,4 @@
 export * from "./useTag";
 export * from "./queries";
 export * from "./TagListProvider";
-export * from "./TagItemListProvider";
 export * from "./TagUpdateProvider";
